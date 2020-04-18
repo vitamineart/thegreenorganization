5d4df7909a49b405049751ad13d229a22518abc6
Art <vitaminearte@gmail.com>
2020-04-18 11:07:32 +0300

    quiz

---
+++ quiz.php
@@ -0,0 +1,246 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Anti-Inflammatory Quiz - Slim Down Smart</title>
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+    <link rel="Shortcut Icon" href="//d2n0w4ubwycugp.cloudfront.net/landings/adwords/favicon.png" type="image/x-icon" />
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//slimdownsmart.com/css/style.css?v2">
+
+	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+	<style>
+	.container{width:970px;}
+	.steps-text{font-size:16px;margin-top: 10px;}
+	.qprogress{
+		background:#EEEEEE;
+		width: 14.2%;
+		padding: 12px 15px;
+		color: #848484;
+	}
+	.qp-done {
+		background: rgb(100, 181, 255) none repeat scroll 0% 0%;
+		color: rgb(255, 255, 255);
+	}
+	.qp-active {
+		background: #0084FF;
+		color:#fff;
+		font-weight:bold;
+	}
+	#form-wrap { margin-top: 30px; }
+	.ques-heading{
+		color: #0058A7;
+		font-size: 27px;
+		margin-bottom: 35px;
+		font-weight:bold;
+		}
+	.qh-small { font-size: 18px; }
+
+	#form-wrap .checkbox label { font-weight: bold;}
+	#form-wrap .checkbox label img {
+		margin-top: -10px;
+		margin-bottom: 10px;
+		margin-left: -20px;
+	}
+	.sub-btn, .sub-btn:hover, .sub-btn:active, .sub-btn:focus, .sub-btn:active:focus {
+		color: rgb(255, 255, 255);
+		background: rgb(243, 128, 21) none repeat scroll 0% 0%;
+		font-weight: bold;
+		font-size: 30px;
+		padding: 6px 114px;
+	}
+	#form-wrap .checks-wrap .checkbox label {
+		font-size: 21px;
+		font-weight: normal;
+		margin-bottom: 5px;
+	}
+	#form-wrap .checks-wrap {
+		margin:0px 0px 10px 30px;
+	}
+	#form-wrap .radio-wrap .radio label {
+		font-size: 21px;
+		font-weight: normal;
+		margin-bottom: 5px;
+	}
+	#form-wrap .radio-wrap {
+		margin:0px 0px 10px 30px;
+	}
+	</style>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-43068296-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+
+<body>
+<div id="header-wrap">
+	<!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand logo" href="#"><img class="img-responsive" src="//slimdownsmart.s3.amazonaws.com/slimdownsmart.jpg" alt="SlimDownSmart"></a>
+          </div>
+
+        </div>
+      </nav>
+</div>
+
+<div id="main-content" class="container">
+	<h1 style="font-size: 30px; margin-top: 25px; font-weight: bold; margin-bottom: 25px;color: #aaa;">How Much <span style="text-decoration: underline;">Inflammation</span> Do You Have?</h1>
+	<div class="clearfix">
+		<div id="qp1" class="col-xs-1 qprogress text-center qp-active">Question 1</div>
+		<div id="qp2" class="col-xs-1 qprogress text-center">Question 2</div>
+		<div id="qp3" class="col-xs-1 qprogress text-center">Question 3</div>
+		<div id="qp4" class="col-xs-1 qprogress text-center">Question 4</div>
+		<div id="qp5" class="col-xs-1 qprogress text-center">Question 5</div>
+		<div id="qp6" class="col-xs-1 qprogress text-center">Question 6</div>
+		<div id="qp7" class="col-xs-1 qprogress text-center">Question 7</div>
+	</div>
+
+	<div id="form-wrap">
+		<form id="quiz-form">
+			<input type="hidden" name="quiz_id" value="q1587123146">
+			<div id="ajax-content">
+
+				<input type="hidden" name="qno" value="1">
+
+				<h2 class="ques-heading">1. What types of foods do you eat on a regular basis? <span class="qh-small">(Select all that apply)</span></h2>
+				<div class="row" style="margin-bottom: 30px;">
+					<div class="col-xs-4">
+						<div class="checkbox">
+							<label>
+								<img src="//slimdownsmart.s3.amazonaws.com/landings/quiz/proteins.jpg"><br>
+								<input type="checkbox" name="ans1" value="yes"> Lean Proteins (Fish, Chicken, Turkey, Beef, Eggs, Steak, etc.)
+							</label>
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="checkbox">
+							<label>
+								<img src="//slimdownsmart.s3.amazonaws.com/landings/quiz/grains.jpg"><br>
+								<input type="checkbox" name="ans2" value="yes"> Grains (Bread, Pasta, Rice, Crackers, etc.)
+							</label>
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="checkbox">
+							<label>
+							<img src="//slimdownsmart.s3.amazonaws.com/landings/quiz/dessert.jpg"><br>
+								<input type="checkbox" name="ans3" value="yes"> Sweets (Cake, Candy, Cookies, Milk Chocolate, etc.)
+							</label>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-xs-4 col-xs-offset-2">
+						<div class="checkbox">
+							<label>
+								<img src="//slimdownsmart.s3.amazonaws.com/landings/quiz/veggies.jpg"><br>
+								<input type="checkbox" name="ans4" value="yes"> Vegetables (Celery, Peppers, Kale, Spinach, etc.)
+							</label>
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<div class="checkbox">
+							<label>
+								<img src="//slimdownsmart.s3.amazonaws.com/landings/quiz/fruits.jpg"><br>
+								<input type="checkbox" name="ans5" value="yes"> Fruits (Blueberries, Strawberries, Bananas, etc.)
+							</label>
+						</div>
+					</div>
+				</div>
+
+
+			</div> <!-- end of ajax content wrap -->
+
+
+			<div style="border-bottom: 2px solid rgb(216, 215, 215); margin: 40px auto; max-width: 800px;"></div>
+			<div class="text-center" style="min-height: 100px;margin-bottom:20px;">
+				<p id="ajax-loading" style="color:#777;display:none;">...Processing...<br><img src="ajax-loader.gif"></p>
+				<p id="ajax-error" style="color:red;display:none;"></p>
+				<button id="submit-btn" type="submit" class="btn btn-default sub-btn">NEXT</button>
+			</div>
+		</form>
+	</div>
+
+</div>
+<script type="text/javascript">
+jQuery(document).ready(function($){
+
+	//progress bar.
+	var qprogress = 1;
+
+	function updateProgressBar() {
+		var t = qprogress;
+		$("#qp" + t).addClass('qp-done')
+		$("#qp" + t).removeClass('qp-active')
+		t = qprogress = qprogress + 1;
+		$("#qp" + t).addClass('qp-active')
+	}
+
+	$("#quiz-form").submit(function(event){
+
+		  event.preventDefault();
+		  $("#submit-btn").hide();
+		  $("#ajax-error").hide();
+		  $("#ajax-loading").show();
+		  $.post( "process.php", $(this).serialize(),
+			function(ajaxdata){
+
+				if(ajaxdata['result'] == "OK") {
+					if(qprogress == 7)
+					window.location = ajaxdata.redirect_url;
+					else {
+						$("#ajax-content").html(ajaxdata.html);
+						updateProgressBar();
+						$("#ajax-loading").hide();
+						$("#submit-btn").show();
+					}
+				}
+				else if(ajaxdata['result'] == "ERROR") {
+					$("#ajax-error").html(ajaxdata['msg']);
+					$("#ajax-error").show();
+					$("#ajax-loading").hide();
+					$("#submit-btn").show();
+				}
+				else {
+					$("#ajax-error").html("Unexpected error occured, Please try again.");
+					$("#ajax-error").show();
+					$("#ajax-loading").hide();
+					$("#submit-btn").show();
+				}
+
+
+			}, 'json');
+
+
+		});
+
+	});
+</script>
+<div class="footer container">
+	<p class="text-center">*Disclaimer: Results may vary from person to person.</p>
+	<p class="text-center footer-links"><a href="/">Slim Down Smart</a> | <a href="/blog">Articles</a> | <a href="/disclaimer">Disclaimer</a> | <a href="/privacy">Privacy Policy</a> | <a href="/tos">T&C</a> |
+	<a href="/about">Author Bio</a> | <a href="/contact">Contact</a></p>
+	<p class="text-center">Copyright: Slim Down Smart Inc.</p>
+
+</div>
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,45 @@
+
+	<header class="entry-header">
+
+		<?php the_title( '<h1 class="title-post entry-title">', '</h1>' ); ?>
+
+
+	</header>
+
+
+<!-- breadcrumb -->
+<?php restoneu_get_breadcrumb() ?>
+
+
+    <div class="restoneu-banner-area">
+		<?php if( is_front_page() ) : ?>
+			<?php
+				$banner_type = get_theme_mod('banner_type', 'image');
+				if ( $banner_type == 'video' && is_front_page() ) :
+					restoneu_banner_video();
+				elseif ( $banner_type == 'carousel' && is_front_page() ) :
+					restoneu_banner_slider();
+				else :
+					restoneu_banner_background();
+				endif;
+			?>
+		<?php else : ?>
+			<div class="other-header">
+				<div class="header-content other">
+					<?php if(function_exists('is_shop')) : ?>
+						<h3 class="title-post entry-title"><?php wp_title(''); ?></h3>
+					<?php else : ?>
+						<h3 class="title-post entry-title"><?php wp_title(''); ?></h3>
+					<?php endif; ?>
+					<hr class="divider-separator"/>
+					<?php if( get_theme_mod('show_breadcrumb', 1) != 0 ): ?>
+					<div class = "breadcrumb" ><?php if (class_exists('WooCommerce')){
+							( (is_woocommerce())? woocommerce_breadcrumb() :restoneu_get_breadcrumb() );
+						}else{
+							restoneu_get_breadcrumb();
+						}?></div>
+					<?php endif; ?>
+				</div>
+			</div>
+		<?php endif; ?>
+	</div>
